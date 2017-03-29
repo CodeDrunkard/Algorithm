63e98b5bdbb459708ae00312abf5bf337f42750b
JT Ma <ma.jiangtao.86@gmail.com>
2017-03-29 15:13:19 +0800

    Add: Contents

---
+++ Algorithm.playground/Pages/Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,184 @@
+/*:
+ 
+ ![Icon](SwiftAlgorithm-410-transp.png)
+ 
+ # Welcome to the Swift Algorithm Club!
+ Learn more in [github.com/raywenderlich/swift-algorithm-club](https://github.com/raywenderlich/swift-algorithm-club)
+ 
+ ## Data Structures
+ 
+ ### Queue
+ 
+ * [Stack]
+ 
+ * [Enum Stack]
+ 
+ * [Queue]
+ 
+ * [Deque]
+ 
+ * [Priority Queue]
+ 
+ * [Ring Buffer]
+ 
+ ### Lists
+ 
+ * [Linked List]
+ 
+ * [Skip-List]
+ 
+ ### Trees
+ 
+ * [Tree]
+ 
+ * [Binary Tree]
+ 
+ * [Binary Search Tree]
+ 
+ * [Red-Black Tree]
+ 
+ * [Splay Tree]
+ 
+ * [Threaded Binary Tree]
+ 
+ * [Segment Tree]
+ 
+ * [kd-Tree]
+ 
+ * [Heap]
+ 
+ * [Fibonacci Heap]
+ 
+ * [Trie]
+ 
+ * [B-Tree]
+ 
+ ### Hashing
+ 
+ * [Hash Table]
+ 
+ * [Hash Functions]
+ 
+ ### Sets
+ 
+ * [Bloom Filter]
+ 
+ * [Hasg Set]
+ 
+ * [Multiset]
+ 
+ * [Ordered Set]
+ 
+ ### Graphs
+ 
+ * [Graph]
+ 
+ * [Breadth-First Search]
+ 
+ * [Depth-First Search]
+ 
+ * [Shortest Path]
+ 
+ * [Single-Source Shortest Paths]
+ 
+ * [Minimum Spanning Tree]
+ 
+ * [All-Pairs Shortest Paths]
+ 
+ ## The algorithms
+ 
+ ### Searching
+ 
+ * [Linear Search]
+ 
+ * [Binary Search]
+ 
+ * [Count Occurrences]
+ 
+ * [Select Minimum/Maximum]
+ 
+ * [k-th Largest Element]
+ 
+ * [Selection Sampling]
+ 
+ * [Union-find]
+ 
+ ### String Search
+ 
+ * [Brute-Force]
+ 
+ * [Boyer-Moore]
+ 
+ * [Knuth-Moore]
+ 
+ * [Rabin-Karp]
+ 
+ * [Longest Common Subsequence]
+ 
+ * [Z-Algorithm]
+ 
+ ### Sorting
+ 
+ * [Insertion Sort]
+ 
+ * [Selection Sort]
+ 
+ * [Shell Sort]
+ 
+ * [Quicksort]
+ 
+ * [Merge Sort]
+ 
+ * [Heap Sort]
+ 
+ * [Counting Sort]
+ 
+ * [Radix Sort]
+ 
+ * [Topological Sort]
+ 
+ * [Bubble Sort]
+ 
+ * [Slow Sort]
+ 
+ ### Compression
+ 
+ * [Run-Length Encoding]
+ 
+ * [Huffman Coding]
+ 
+ ### Miscellaneous
+ 
+ * [Shuffle]
+ 
+ * [Comb Sort]
+ 
+ ### Mathematics
+ 
+ * [Greatest Common Divisor]
+ 
+ * [Permutations and Combinations]
+ 
+ * [Shunting Yard Algorithm]
+ 
+ * [Statistics]
+ 
+ * [Karatsuba Multiplication]
+ 
+ * [Haversine Distance]
+ 
+ ### Machine learning
+ 
+ * [k-Means Clustering]
+ 
+ * [k-Nearest Neighbors]
+ 
+ * [Linear regression]
+ 
+ * [Logistic Regression]
+ 
+ * [Neural Networks]
+ 
+ * [PageRank]
+ 
+ */
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Algorithm
-Algorithm learning
+
+## Purpose
+Algorithm learning in Swift with playground, more information in [github.com/raywenderlich/swift-algorithm-club](https://github.com/raywenderlich/swift-algorithm-club)
+
+## License
+All content is licensed under the terms of the [MIT](LICENSE) open source license.
